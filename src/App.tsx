b29870aedcdd72eaf6f60063794228e8cebc690b
Marat Zh <zmhan.box@gmail.com>
2021-05-16 00:53:25 +0500

    Added Add and Remove buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,16 @@ import './App.css';
 
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import React, { useState } from 'react';
+import React from 'react';
 
 import RegistrationForm from './component/RegistrationForm';
 
 const App: any = () => {
-  const [data, setData] = useState<string>('');
-
-  const callback = (jsonStr: string) => {
-    setData(jsonStr);
-  };
-
   return (
     <div className="App">
       <CssBaseline />
       <Container maxWidth="sm">
-        <RegistrationForm callback={callback} />
-        <code>{data}</code>
+        <RegistrationForm />
       </Container>
     </div>
   );
--- src/component/RegistrationForm.tsx
+++ src/component/RegistrationForm.tsx
@@ -3,19 +3,41 @@ import './RegistrationStyle.css';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import { useState } from 'react';
 
-const RegistrationForm = ({ callback }: any) => {
+const RegistrationForm = () => {
+  const [field, setField] = useState([
+    {
+      id: 1
+    }
+  ]);
   const onRegister = (e: any) => {
     e.preventDefault();
+  };
 
-    const prepareData = {
-      email: e.target.elements.email.value,
-      firstName: e.target.elements.firstName.value,
-      lastName: e.target.elements.lastName.value,
-      password: e.target.elements.password.value,
-    };
+  const addNewFiled = () => {
+    console.log('New Fielf');
+    try {
+      let counter = field[field.length - 1].id;
+      counter++;
+      const newArray = [];
+      field.forEach((item) => {
+        newArray.push(item);
+      });
+      newArray.push({ id: counter });
+      setField(newArray);
+    } catch (error) {
+      let counter = 0;
+      counter++;
+      const newArray = [];
+      newArray.push({ id: counter });
+      setField(newArray);
+    }
+  };
 
-    callback(JSON.stringify(prepareData));
+  const removeFiledById = (id: number) => {
+    const find = field.filter((item) => item.id !== id);
+    setField(find);
   };
 
   return (
@@ -23,15 +45,22 @@ const RegistrationForm = ({ callback }: any) => {
       <Typography component="div" variant="h5">
         Enter to Site
       </Typography>
-      <TextField fullWidth id="email" label="Email Address" variant="outlined" />
-      <TextField fullWidth id="firstName" label="First Name" variant="outlined" />
-      <TextField fullWidth id="lastName" label="Last Name" variant="outlined" />
-      <TextField fullWidth id="password" label="Password" variant="outlined" />
-      <Typography align="left">
-        <Button color="primary" type="submit" variant="outlined">
-          Register
+      {field.map((item) => {
+        return (
+          <div className="My-field" key={item.id}>
+            <TextField fullWidth id="email" label="Email Address" variant="outlined" />
+            <Button color="secondary" variant="outlined" onClick={() => removeFiledById(item.id)}>
+              Remove
+            </Button>
+          </div>
+        );
+      })}
+
+      <div className="My-field">
+        <Button color="primary" variant="outlined" onClick={() => addNewFiled()}>
+          Add
         </Button>
-      </Typography>
+      </div>
     </form>
   );
 };
